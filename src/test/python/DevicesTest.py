35624ebbf7a08a57da7b5a311c6c83a0dbfb43be
werpu <werner.punz@gmail.com>
2019-05-30 20:26:18 +0200

    basic device parsing tests now working, this suffices for my personal usecases

--- src/test/python/DevicesTest.py
+++ src/test/python/DevicesTest.py
@@ -6,15 +6,13 @@ from utils.config import Config
 
 class MyTestCase(unittest.TestCase):
 
-    def test_deviceparsing(self):
+    def test_device_parsing(self):
 
         devices = DevicesMock(Config("../resources/devices.yaml"))
         self.assertTrue(len(devices.devices) == 3)
         self.assertEqual(devices.devices[2].name, 'Ultimarc UltraStik Ultimarc UltraStik Player 2')
         self.assertEqual(devices.devices[1].name, 'Ultimarc UltraStik Ultimarc UltraStik Player 1')
         self.assertEqual(devices.devices[0].name, 'Ultimarc I-PAC Ultimarc I-PAC')
-
-
 
 
 if __name__ == '__main__':
