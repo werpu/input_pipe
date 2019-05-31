80c12f7b74f3b15391d420e0cd0b878aac7d99eb
werpu <werner.punz@gmail.com>
2019-05-31 13:33:42 +0200

    preparations for the config device creation

--- src/test/python/TargetDevicesTest.py
+++ src/test/python/TargetDevicesTest.py
@@ -3,24 +3,34 @@ import unittest
 from devices.drivers.keybd import VirtualKeyboard
 from devices.drivers.mouse import VirtualMouse
 from devices.drivers.xbx360 import Xbox360
+from devices.targetdevices import TargetDevices
+from utils.config import Config
 from utils.evdevutils import EvDevUtils
 from time import sleep
 
 
+# Tests for the target device capabilities
+# We test the driver generation the target device matching
+# and the event sendinng here
 class TargetDevTestCase(unittest.TestCase):
 
     def test_device_creation(self):
-        self.assert_device(Xbox360())
-        self.assert_device(VirtualMouse())
-        self.assert_device(VirtualKeyboard())
+        self._assert_created_device(Xbox360())
+        self._assert_created_device(VirtualMouse())
+        self._assert_created_device(VirtualKeyboard())
+
+    # config test for matching devices after creation
+    def test_device_match(self):
+        devices = TargetDevices(Config("../resources/devices.yaml"))
+        print("todo implement me")
 
     # Generic dives creation assert routine
-    def assert_device(self, device_drv):
+    def _assert_created_device(self, device_drv):
         device_drv.create().syn()
         found = False
         cnt = 0
         # give linux time to catch up usually 0.05sec on a semi modern machine
-        while cnt < 3 and found is False:
+        while cnt < 3 and not found:
             cnt += 1
             sleep(0.1)
             for dev in EvDevUtils.get_available_devices():
