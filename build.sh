336c9c36c06e31df671f4e7ba41807a097ed55ce
werpu <werner.punz@gmail.com>
2019-06-04 20:42:34 +0200

    optimizing build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd src/main/python
 export PIPENV_IGNORE_VIRTUALENVS=1
-pipenv run pyinstaller --onefile --distpath ../../../dist/ ./input_pipe.py
+pipenv run pyinstaller -s  -n input_pipe --onefile --distpath ../../../dist/ ./input_pipe.py
