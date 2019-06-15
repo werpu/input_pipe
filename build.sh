4dabbe6d98b24b9ab0ecbd26f0200a1da608965b
Werner Punz <werner.punz@gmail.com>
2019-06-15 22:10:54 +0200

    adding a requiremnts.txt file

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 #export PIPENV_IGNORE_VIRTUALENVS=1
 rm ./dist/input_pipe
 pipenv run pyinstaller -s  -n input_pipe --onefile --distpath ./dist/ ./src/main/python/input_pipe.py
+pip freeze > requirements.txt
