6edac4a77914ce9566e2b54a1ac648902c80b9ac
Werner Punz <werner.punz@gmail.com>
2020-02-17 21:59:29 +0100

    enabling the spinner

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 #export PIPENV_IGNORE_VIRTUALENVS=1
-pipenv run python ./src/main/python/input_pipe.py -c ./src/main/resources/config_examples/devices-serial.json5.vm
+pipenv run python ./src/main/python/input_pipe.py -c /home/pi/development/input_pipe/src/main/resources/config_examples/devices-serial.json5.vm
 
 
