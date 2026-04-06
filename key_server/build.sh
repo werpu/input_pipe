07735ac8ea3db6caeac73b4b55c307ef2144b15a
werpu <werner.punz@gmail.com>
2026-04-06 10:58:37 +0200

    fixing builds

--- key_server/build.sh
+++ key_server/build.sh
@@ -1,4 +1,7 @@
-y#!/usr/bin/env bash
+#!/usr/bin/env bash
+python3 -m venv .venv
+source .venv/bin/activate
 pip install evdev pyinstaller
 pyinstaller -s -n key_server --onefile --distpath ./dist/ key_server.py
 pyinstaller -s -n key_client --onefile --distpath ./dist/ key_client.py
+deactivate
--- key_server_mac/build.sh
+++ key_server_mac/build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+python3 -m venv .venv
+source .venv/bin/activate
 pip install pynput pyinstaller
 pyinstaller -s -n key_server --onefile --distpath ./dist/ key_server.py
 pyinstaller -s -n key_client --onefile --distpath ./dist/ key_client.py
+deactivate
